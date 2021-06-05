d2ed25303ad884c0983fc38e8fad08a709e94389
bob <657793418@qq.com>
2021-06-05 23:24:17 +0800

    完成windowsqt的环境搭建和钻石绘画 这次实验给我一个启发 就是一定要看example 太有用了

---
+++ src/DiamondbyQt+glfwOnWindows/main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.setWindowTitle("图形学实验");
+
+    w.show();
+    return a.exec();
+}
---
+++ src/DiamondbyQt+glfwOnWindows/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "Diamond.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QMenuBar * menubar;
+    MainWindow * parentWindow;
+    int menubarHeight;
+    int windowsHeight;
+    int windowsWidth;
+//    Diamond_dynamic *dd;
+    Diamond *d;
+private slots:
+    void resizeOpenGLWindow();
+    void drawDiamond();
+    void showAbout();
+};
+#endif // MAINWINDOW_H
